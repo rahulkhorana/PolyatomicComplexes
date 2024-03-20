354609e5ebe444d59fa1beb6848e68757c8fb135
rahulkhorana <rahulkhorana7170580@gmail.com>
2024-03-20 09:32:52 -0700

    lookup

--- src/build_atom_lookup.py
+++ src/build_atom_lookup.py
@@ -1,15 +1,33 @@
 import os
 import json
+import dill
+from collections import defaultdict
+from atomic_complex import AtomComplex
 
 class BuildAtoms():
    
+   def __init__(self):
+       self.cwd = os.getcwd()
+       self.datapath = self.cwd + '/dataset/construct'
+   
    def build_lookup_table(self) -> None:
-        assert 'construct/lookup_map.json' in os.listdir('/dataset/')
-        with open('/dataset/construct/lookup_map.json') as data:
-            d = json.loads(data)
+        assert 'lookup_map.json' in os.listdir(self.datapath)
+        with open(self.datapath+'/lookup_map.json') as data:
+            d = json.load(data)
         
-        print(d)
-        return
+        lookup_table = defaultdict(list)
+
+        for i, element in enumerate(d):
+            n_protons, n_neutrons, n_electrons = d[element]
+            ac = AtomComplex(n_protons, n_neutrons, n_electrons, 5, 3, 3, 0)
+            complex = ac.fast_build_complex()
+            print(f'finished {i}')
+            lookup_table[element] = complex
+        
+        with open(self.datapath+'/atom_lookup.pkl', 'wb') as f:
+            dill.dump(lookup_table, f)
+        
+        return None
 
 
 if __name__ == '__main__':
