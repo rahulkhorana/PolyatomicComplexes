0d0fd5fe5d0d1c3a0458e0916a6a50476d9093c1
rahulkhorana <rahulkhorana7170580@gmail.com>
2024-03-20 11:49:49 -0700

    complex works

--- src/polyatomic_complex.py
+++ src/polyatomic_complex.py
@@ -3,8 +3,9 @@ import dill
 import json
 import numpy as np
 import jax.numpy as jnp
-from typing import Tuple
+from typing import List, Tuple
 from core_utils import GluingMap, ElectronField
+from building_blocks import Electron
 
 class PolyAtomComplex():
 
@@ -32,6 +33,14 @@ class PolyAtomComplex():
             assert hasattr(update_forces, '__call__')
             self.update_forces = update_forces
     
+    def gen_elec_info(self, k) -> List:
+        self.AE = []
+        self.d_e = 0
+        if isinstance(self.d_e,int):
+            electrons =  Electron(self.d_e, k).build_electron()
+            self.AE = list(electrons)
+        return self.AE
+    
     def fast_build_complex(self) -> Tuple:
         lookup = self.datapath+'/atom_lookup.pkl'
         with open(lookup, 'rb') as f:
@@ -39,7 +48,7 @@ class PolyAtomComplex():
         C = []
         for a in self.atoms:
             acomplex = lookup[a]
-            C.append(acomplex[0])
+            C += list(acomplex[0].ravel())
         C = np.asarray(C)
         return tuple([C, [], [], []])
     
@@ -47,14 +56,17 @@ class PolyAtomComplex():
         lookup = self.datapath+'/atom_lookup.pkl'
         with open(lookup, 'rb') as f:
             lookup = dill.load(f)
+        eleminfo = self.lookup_map
         A = []
-        for a in enumerate(self.atoms):
+        for i, a in enumerate(self.atoms):
             acomplex = lookup[a]
-            A.append(acomplex)
+            inf = eleminfo[a]
+            info = [inf[2], acomplex[0], acomplex[1], acomplex[2], acomplex[3]]
+            A.append(info)
         C = np.asarray([])
         self.E = ElectronField()
-        self.F = np.matrix()
-        self.DE = np.matrix()
+        self.F = np.array([])
+        self.DE = np.array([])
 
         def disjont_union(gluing_map:np.ndarray, skeleton:np.ndarray, particle:np.ndarray) -> np.ndarray:
             skeleton = np.concatenate([skeleton.ravel(), gluing_map.ravel()])
@@ -63,12 +75,12 @@ class PolyAtomComplex():
             return K
 
         def electron_union(EF_1:tuple, EF_2:tuple) -> None:
-            ee0_1, ee0_2 = EF_1[0], EF_2[0]
-            wv_1, wv_2 = EF_1[1], EF_2[1]
-            assert isinstance(ee0_1,np.ndarray) and isinstance(ee0_2,np.ndarray)
-            assert isinstance(wv_1,np.ndarray) and isinstance(wv_2,np.ndarray)
-            e_field = np.union1d(ee0_1, ee0_2)
-            wves = np.concatenate([wv_1.ravel(), wv_2.ravel()])
+            ee0_1 = EF_1[0]
+            wv_1 = EF_1[1]
+            assert isinstance(ee0_1,np.ndarray)
+            assert isinstance(wv_1,np.ndarray) 
+            e_field = np.union1d(ee0_1, [])
+            wves = np.concatenate([wv_1.ravel()])
             e_field, waves = e_field.ravel(), wves.ravel()
             self.E.add_electrons(e_field, waves)
             return None
@@ -81,14 +93,15 @@ class PolyAtomComplex():
         
         while A:
             I = A.pop(0)
-            k, ae, df, de = I[0]
+            num_elec, k, ae, df, de = I
+            if len(C) == 0:
+                C = k
             phi = GluingMap(C, k, target=C).construct_map()
             map = np.asarray(list(phi.keys()), dtype=jnp.float32)
             map = np.unique(map)
-            if len(C) == 0:
-                C = k
             C = disjont_union(map, C, k)
-            electron_union(self.E, ae)
+            res = self.gen_elec_info(num_elec)
+            electron_union(res, [])
             if self.using_force:
                 self.F = dir_sum(self.F, df)
                 self.update_forces(self.F)
@@ -96,7 +109,7 @@ class PolyAtomComplex():
                 self.DE = dir_sum(self.DE, de)
                 self.update_radial(self.DE)
         
-        return tuple([C, self.E, self.F, self.DE])
+        return tuple([C, self.E.get_efield(), self.F, self.DE])
 
 def sanity_test(atom_list, kind):
     pac = PolyAtomComplex(atom_list)
@@ -117,4 +130,8 @@ def sanity_test(atom_list, kind):
 
 if __name__ == '__main__':
     sanity_test(['H', 'H', 'O'], '')
+    sanity_test(['C', 'H', 'H', 'H'], '')
+    sanity_test(['Np','U', 'P', 'P', 'P', 'P', 'H', 'H', 'H', 'H', 'C', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'C', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'], '')
     sanity_test(['H', 'H', 'O'], 'general')
+    sanity_test(['C', 'H', 'H', 'H'], 'general')
+    sanity_test(['Np','U', 'P', 'P', 'P', 'P', 'H', 'H', 'H', 'H', 'C', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'C', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'], 'general')
