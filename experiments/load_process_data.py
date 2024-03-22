693056e554e6b48de6a07e5db467928ee09063cf
rahulkhorana <rahulkhorana7170580@gmail.com>
2024-03-22 14:21:54 -0700

    photoswitches benchmark done

--- experiments/load_process_data.py
+++ experiments/load_process_data.py
@@ -1,18 +1,17 @@
-import os
 import torch
 import dill
-import numpy as np
 import pandas as pd
-from typing import Tuple, Optional
-from torch.utils.data import TensorDataset
+from typing import Tuple
+from gauche.dataloader import MolPropLoader
 
 class LoadDatasetForTask():
-    def __init__(self, X, y, repn):
+    def __init__(self, X, y, y_column, repn):
         self.X = X
         self.y = y
+        self.y_column = y_column
         self.repn = repn
     
-    def load(self) -> Tuple[torch.Tensor, torch.Tensor]:
+    def load_photoswitches(self) -> Tuple[torch.Tensor, torch.Tensor]:
         if self.repn == 'complexes':
             with open(self.X, 'rb') as f:
                 x_data = dill.load(f)
@@ -25,14 +24,50 @@ class LoadDatasetForTask():
             data = [torch.nn.functional.pad(x, pad=(0, max_len - x.numel()), mode='constant', value=0) for x in X]
             X = torch.stack(data)
             ydata = pd.read_csv(self.y)
-            y = ydata['E isomer n-pi* wavelength in nm']
+            y = ydata[self.y_column]
             mean_value = y.mean()
             y.fillna(value=mean_value, inplace=True)
             y = torch.tensor(y.values).view(len(y), 1)
             assert len(X) == len(y) and isinstance(X, torch.Tensor) and isinstance(y, torch.Tensor)
             return tuple([X, y])
-
-
-if __name__ == '__main__':
-    ds = LoadDatasetForTask(X='dataset/photoswitches/fast_complex_lookup_repn.pkl', y='dataset/photoswitches/photoswitches.csv', repn='complexes')
-    res = ds.load()
+        elif self.repn == 'fingerprints':
+            loader = MolPropLoader()
+            loader.validate = lambda: False
+            loader.load_benchmark("Photoswitch")
+            loader.featurize('ecfp_fragprints')
+            X = loader.features
+            X = torch.from_numpy(X).type(torch.float64)
+            ydata = pd.read_csv(self.y)
+            y = ydata[self.y_column]
+            mean_value = y.mean()
+            y.fillna(value=mean_value, inplace=True)
+            y = torch.tensor(y.values).view(len(y), 1)
+            assert len(X) == len(y) and isinstance(X, torch.Tensor) and isinstance(y, torch.Tensor)
+            return tuple([X, y])
+        elif self.repn == 'SELFIES':
+            loader = MolPropLoader()
+            loader.validate = lambda: False
+            loader.load_benchmark("Photoswitch")
+            loader.featurize('bag_of_selfies')
+            X = loader.features
+            X = torch.from_numpy(X).type(torch.float64)
+            ydata = pd.read_csv(self.y)
+            y = ydata[self.y_column]
+            mean_value = y.mean()
+            y.fillna(value=mean_value, inplace=True)
+            y = torch.tensor(y.values).view(len(y), 1)
+            assert len(X) == len(y) and isinstance(X, torch.Tensor) and isinstance(y, torch.Tensor)
+            return tuple([X, y])
+        elif self.repn == 'GRAPHS':
+            loader = MolPropLoader()
+            loader.validate = lambda: False
+            loader.load_benchmark("Photoswitch")
+            loader.featurize('molecular_graphs')
+            X = loader.features
+            ydata = pd.read_csv(self.y)
+            y = ydata[self.y_column]
+            mean_value = y.mean()
+            y.fillna(value=mean_value, inplace=True)
+            y = torch.tensor(y.values).view(len(y), 1)
+            assert len(X) == len(y) and isinstance(y, torch.Tensor)
+            return tuple([X, y])
--- experiments/photoswitches_experiment.py
+++ experiments/photoswitches_experiment.py
@@ -16,12 +16,9 @@ from gpytorch.likelihoods import GaussianLikelihood
 from gpytorch.mlls import ExactMarginalLogLikelihood
 
 # kernels + gp
-from kernels import TanimotoKernel, WalkKernel, GraphKernel
-from gaussian_process import run_training_loop, evaluate_model
-
-# gauche libraries
-sys.path.append('.')
-from gauche_utils.data_utils import transform_data
+from kernels import TanimotoKernel
+from gaussian_process import evaluate_model, evaluate_graph_model
+from gauche import SIGP
 
 from matplotlib import pyplot as plt
 
@@ -44,90 +41,87 @@ class ExactGPModel(ExactGP):
         covar_x = self.covar_module(x)
         return MultivariateNormal(mean_x, covar_x)
 
-class GP(ExactGP):
-    def __init__(self, train_X, train_Y, likelihood):
-        assert type(ENCODING) is str and len(ENCODING) > 0
-        super(ExactGP).__init__(train_X, train_Y, likelihood)
-        if ENCODING == 'complexes':
-            return
-        elif ENCODING != 'graphs':
-            self.mean_module = ConstantMean()
-            self.covar_module = ScaleKernel(base_kernel=TanimotoKernel())
-            self.to(device)
-        else:
-            self.mean_module = ConstantMean()
-            self.covar_module = ScaleKernel(base_kernel=GraphKernel())
-            self.to(device)
+class GraphGP(SIGP):
+    def __init__(self, train_x, train_y, likelihood, kernel, **kernel_kwargs):
+        super().__init__(train_x, train_y, likelihood)
+        self.mean = ConstantMean()
+        self.covariance = kernel(**kernel_kwargs)
 
     def forward(self, x):
-        mean_x = self.mean_module(x)
-        covar_x = self.covar_module(x)
-        return MultivariateNormal(mean_x, covar_x)
+        """
+        A forward pass through the model.
+        """
+        mean = self.mean(torch.zeros(len(x), 1)).float()
+        covariance = self.covariance(x)
+
+        # because graph kernels operate over discrete inputs it is beneficial
+        # to add some jitter for numerical stability
+        jitter = max(covariance.diag().mean().detach().item() * 1e-4, 1e-4)
+        covariance += torch.eye(len(x)) * jitter
+        return MultivariateNormal(mean, covariance)
 
 
-def initialize_model(train_x:torch.Tensor, train_obj:torch.Tensor, likelihood, state_dict=None):
+def initialize_model(train_x:torch.Tensor, train_obj:torch.Tensor, likelihood):
     model = ExactGPModel(train_x, train_obj, likelihood).to(train_x)
     return model
+
+def initialize_graph_gp(train_x, train_obj, likelihood, kernel, **kernel_kwargs):
+    model  = GraphGP(train_x, train_obj, likelihood, kernel, **kernel_kwargs)
+    return model
+
+def one_experiment(target, encoding, n_trials, n_iters):
+    X,y = [], []
+    if encoding == 'complexes':
+        X,y = LoadDatasetForTask(X='dataset/photoswitches/fast_complex_lookup_repn.pkl', y='dataset/photoswitches/photoswitches.csv', repn=encoding, y_column=target).load_photoswitches()
+    elif ENCODING == 'fingerprints':
+            X,y = LoadDatasetForTask(X='gauche_ecfp', y='dataset/photoswitches/photoswitches.csv', repn=encoding, y_column=target).load_photoswitches()
+    elif ENCODING == 'SELFIES':
+        X,y = LoadDatasetForTask(X='gauche_selfies', y='dataset/photoswitches/photoswitches.csv', repn=encoding, y_column=target).load_photoswitches()
+    elif ENCODING == 'GRAPHS':
+        X,y = LoadDatasetForTask(X='gauche_graphs', y='dataset/photoswitches/photoswitches.csv', repn=encoding, y_column=target).load_photoswitches()
+    
+    if ENCODING != 'GRAPHS':
+        r2_list,rmse_list, mae_list, confidence_percentiles, mae_mean, mae_std = evaluate_model(initialize_model=initialize_model, n_trials=n_trials, n_iters=n_iters, test_set_size=holdout_set_size, X=X, y=y, figure_path=f'results/{EXPERIMENT_TYPE}/confidence_mae_model_{ENCODING}_{target}.png')
+    else:
+        r2_list,rmse_list, mae_list, confidence_percentiles, mae_mean, mae_std = evaluate_graph_model(initialize_graph_gp, n_trials=n_trials, n_iters=n_iters, test_set_size=holdout_set_size, X=X, y=y, figure_path=f'results/{EXPERIMENT_TYPE}/confidence_mae_model_{ENCODING}_{target}.png')
+    
+    mean_r2 = "\nmean R^2: {:.4f} +- {:.4f}".format(np.mean(r2_list), np.std(r2_list)/np.sqrt(len(r2_list)))
+    mean_rmse = "mean RMSE: {:.4f} +- {:.4f}".format(np.mean(rmse_list), np.std(rmse_list)/np.sqrt(len(rmse_list)))
+    mean_mae = "mean MAE: {:.4f} +- {:.4f}\n".format(np.mean(mae_list), np.std(mae_list)/np.sqrt(len(mae_list)))
+    return mean_r2, mean_rmse, mean_mae
 
 
 if __name__ == '__main__':
     EXPERIMENT_TYPE = 'Photoswitches'
-    ENCODING = 'complexes'
-    N_TRIALS = 5
+    ENCODING = 'GRAPHS'
+    N_TRIALS = 20
     N_ITERS = 5
     holdout_set_size = 0.33
     # dataset processing
     X,y = [], []
     # dataset loading
-    if ENCODING == "complexes":
-        ds = LoadDatasetForTask(X='dataset/photoswitches/fast_complex_lookup_repn.pkl', y='dataset/photoswitches/photoswitches.csv', repn=ENCODING)
-        X,y = ds.load()
-    elif ENCODING == "SELFIES" or ENCODING == 'fingerprints' or ENCODING == 'GRAPHS':
-        X,y = LoadDatasetForTask(Xpath='', yPath='', repn=ENCODING).load()
-    else:
-        raise Exception("unsupported")
+    possible_target_cols = ['rate of thermal isomerisation from Z-E in s-1','Z PhotoStationaryState','E PhotoStationaryState','E isomer pi-pi* wavelength in nm','Extinction','E isomer n-pi* wavelength in nm',
+    'Extinction coefficient in M-1 cm-1','Z isomer pi-pi* wavelength in nm','Extinction.1','Z isomer n-pi* wavelength in nm','Extinction coefficient in M-1 cm-1.1','Wiberg index',
+    'PBE0 DFT E isomer pi-pi* wavelength in nm','PBE0 DFT E isomer n-pi* wavelength in nm','PBE0 DFT Z isomer pi-pi* wavelength in nm','PBE0 DFT Z isomer n-pi* wavelength in nm']
+
+    results = []
     
-    print(f'tpes {type(X)}')
-    print(f'tpes {type(y)}')
-    # training
-    r2_list,rmse_list, mae_list, confidence_percentiles, mae_mean, mae_std = evaluate_model(initialize_model=initialize_model,n_trials=N_TRIALS, n_iters=N_ITERS, test_set_size=holdout_set_size, X=X, y=y, figure_path=f'results/{EXPERIMENT_TYPE}/{time.time()}_confidence_mae_model_{ENCODING}.png')
+    for col in possible_target_cols:
+        mean_r2, mean_rmse, mean_mae = one_experiment(col, ENCODING,N_TRIALS,N_ITERS)
+        results.append([col, mean_r2, mean_rmse, mean_mae])
+
 
     if type(EXPERIMENT_TYPE) is str:
         trial_num = len(os.listdir(f'results/{EXPERIMENT_TYPE}'))
-        results_path = f"results/{EXPERIMENT_TYPE}/result_trial_{trial_num}_{time.time()}.txt"
-        
-        mean_r2 = "\nmean R^2: {:.4f} +- {:.4f}".format(np.mean(r2_list), np.std(r2_list)/np.sqrt(len(r2_list)))
-        mean_rmse = "mean RMSE: {:.4f} +- {:.4f}".format(np.mean(rmse_list), np.std(rmse_list)/np.sqrt(len(rmse_list)))
-        mean_mae = "mean MAE: {:.4f} +- {:.4f}\n".format(np.mean(mae_list), np.std(mae_list)/np.sqrt(len(mae_list)))
+        results_path = f"results/{EXPERIMENT_TYPE}/{ENCODING}_{time.time()}.txt"
 
         with open(results_path, 'w') as f:
             f.write(EXPERIMENT_TYPE + ':')
             f.write('\n')
             f.write(ENCODING + ':')
-            f.write('\n')
-            f.write(mean_r2)
-            f.write('\n')
-            f.write(mean_rmse)
-            f.write('\n')
-            f.write(mean_mae)
-            f.write('\n')
-            f.write('r^2 list: \n')
-            np.savetxt(f, r2_list, delimiter=',')
-            f.write('\n')
-            f.write('rmse list: \n')
-            np.savetxt(f, rmse_list, delimiter=',')
-            f.write('\n')
-            f.write('mae list: ')
-            np.savetxt(f, mae_list, delimiter=',')
-            f.write('\n')
-            f.write('confidence percentiles: \n')
-            np.savetxt(f, confidence_percentiles, delimiter=',')
-            f.write('\n')
-            f.write('mae mean: \n')
-            np.savetxt(f, mae_mean, delimiter=',')
-            f.write('\n')
-            f.write('mae std: \n')
-            np.savetxt(f, mae_std, delimiter=',')
+            for result in results:
+                col, mean_r2, mean_rmse, mean_mae = result
+                f.write(f'column: {col}, {mean_r2}, {mean_rmse}, {mean_mae}')
+                f.write('\n')
         f.close()
-        
         print("CONCLUDED")
