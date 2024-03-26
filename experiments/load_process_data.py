8cacc6eca0997b9b212a5d61c6b51a508c3a93a5
rahulkhorana <rahulkhorana7170580@gmail.com>
2024-03-25 18:49:55 -0700

    update mp

--- experiments/load_process_data.py
+++ experiments/load_process_data.py
@@ -425,3 +425,30 @@ class LoadDatasetForTask:
             y = torch.tensor(y.values).view(len(y), 1)
             assert len(X) == len(y) and isinstance(y, torch.Tensor)
             return tuple([X, y])
+
+    def load_mp(self):
+        if self.repn == "complexes":
+            with open(self.X, "rb") as f:
+                x_data = dill.load(f)
+            X = []
+            for x in x_data:
+                rep = x_data[x][0]
+                t = torch.tensor(rep)
+                X.append(t)
+            max_len = max([x.squeeze().numel() for x in X])
+            data = [
+                torch.nn.functional.pad(
+                    x, pad=(0, max_len - x.numel()), mode="constant", value=0
+                )
+                for x in X
+            ]
+            X = torch.stack(data)
+            ydata = pd.read_csv(self.y)
+            y = ydata[self.y_column]
+            y = torch.tensor(y.values).view(len(y), 1)
+            assert (
+                len(X) == len(y)
+                and isinstance(X, torch.Tensor)
+                and isinstance(y, torch.Tensor)
+            )
+            return tuple([X, y])
