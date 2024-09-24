0d83da319fc4a376be49e209609d304b5b0f4654
rahulkhorana <rahulkhorana7170580@gmail.com>
2024-09-24 14:11:40 -0700

    packaging

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 if __name__ == "__main__":
     setup(
         name="polyatomic_complexes",
-        version="0.0.2",
+        version="0.0.3",
         url="https://github.com/rahulkhorana/PolyatomicComplexes",
         author="Rahul Khorana",
         packages=find_packages(),
@@ -223,11 +223,5 @@ if __name__ == "__main__":
             "zope.interface==6.2",
         ],
         include_package_data=True,
-        package_data={
-            "": [
-                "PolyatomicComplexes/polyatomic_complexes/dataset/*.csv",
-                "PolyatomicComplexes/polyatomic_complexes/dataset/*.pkl",
-                "PolyatomicComplexes/polyatomic_complexes/dataset/*.json",
-            ]
-        },
+        package_data={"": ["dataset/*"]},
     )
