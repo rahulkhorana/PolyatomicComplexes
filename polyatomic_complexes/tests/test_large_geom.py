f87f9515d05c85cc8b5402c8e43a02768f8a32e6
Rahul Khorana <rahulkhorana7170580@gmail.com>
2025-01-27 09:36:25 +0000

    fewer number of tests for actions

--- polyatomic_complexes/tests/test_large_geom.py
+++ polyatomic_complexes/tests/test_large_geom.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pytest
+import numpy as np
 import pandas as pd
 from pathlib import Path
 
@@ -52,6 +53,7 @@ smiles_freesolv = pd.read_csv(datapath_freesolv)["smiles"].tolist()
 smiles_lipo = pd.read_csv(datapath_lipo)["smiles"].tolist()
 smiles_photo = pd.read_csv(datapath_photo)["SMILES"].tolist()
 ALL_SMILES = smiles_esol + smiles_freesolv + smiles_lipo + smiles_photo
+ALL_SMILES = np.random.choice(a=ALL_SMILES, size=30, replace=False).tolist()
 
 large_cases = []
 for smile in ALL_SMILES:
