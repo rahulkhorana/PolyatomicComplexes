d8b3e651792e127fd00baf4b1742fec25aa0376a
Rahul Khorana <rahul.khorana.email@gmail.com>
2025-01-28 15:42:58 +0000

    reducing

--- polyatomic_complexes/tests/test_large_geom.py
+++ polyatomic_complexes/tests/test_large_geom.py
@@ -53,7 +53,7 @@ smiles_freesolv = pd.read_csv(datapath_freesolv)["smiles"].tolist()
 smiles_lipo = pd.read_csv(datapath_lipo)["smiles"].tolist()
 smiles_photo = pd.read_csv(datapath_photo)["SMILES"].tolist()
 ALL_SMILES = smiles_esol + smiles_freesolv + smiles_lipo + smiles_photo
-ALL_SMILES = np.random.choice(a=ALL_SMILES, size=10, replace=False).tolist()
+ALL_SMILES = np.random.choice(a=ALL_SMILES, size=5, replace=False).tolist()
 
 large_cases = []
 for smile in ALL_SMILES:
@@ -94,8 +94,8 @@ def test_quantum_polyatomic_geometry(smile, mode):
 
 #### LARGE UNIQUE ####
 large_cases_pair = []
-for smile1 in ALL_SMILES[:20]:
-    for smile2 in ALL_SMILES[:20]:
+for smile1 in ALL_SMILES[:5]:
+    for smile2 in ALL_SMILES[:5]:
         if smile1 != smile2:
             for mode in modes:
                 large_cases_pair.append((smile1, smile2, mode))
